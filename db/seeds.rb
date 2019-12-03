bb9532cc85e4e25bd017eec2835ad629d5c36d4f
Mihcael <michael@aceworld.com.au>
2019-12-04 00:30:19 +1100

    adding a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Currency.create(name: "BTCAUD", endpoint: "https://data.exchange.coinjar.com/products/BTCAUD/ticker")
+Currency.create(name: "ETHAUD", endpoint: "https://data.exchange.coinjar.com/products/ETHAUD/ticker")
